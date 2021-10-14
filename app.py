0c2209ba5be5cce4404046ed50546e659fbceade
Massimiliano Riposati <38966126+maxriposati@users.noreply.github.com>
2021-10-14 16:02:18 +0200

    Spring 3
    Spring 3

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, request
+from forms import FormContacto,FormLogin,FormRestablecer,FormRetroalimentacion,FormUsuario,FormUsuarioFinal
+import os
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(24)
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html', titulo="home")
+
+@app.route('/mision')
+def mision():
+    return render_template('mision.html', titulo="mision")
+
+@app.route('/vision')
+def vision():
+    return render_template('vision.html', titulo="vision")
+
+@app.route('/servicios')
+def servicios():
+    return render_template('servicios.html', titulo="servicios")
+
+@app.route('/contacto', methods=["GET","POST"])
+def contacto():
+    formcontacto = FormContacto()
+    return render_template('contacto.html', titulo="contacto", form=formcontacto)
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    formlogin = FormLogin()
+    formFinalUser = FormUsuarioFinal()
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username=="admin" and password=="admin":
+            return render_template('mainadmin.html', titulo="main admininstrador")
+        elif username=="user" and password=="user":
+            id = 1
+            return render_template('mainusuario.html', titulo="main usuario", id=id, form=formFinalUser)
+        else:
+            return render_template('login.html', titulo="login", form=formlogin)
+            
+    return render_template('login.html', titulo="login", form=formlogin)
+            
+            
+
+@app.route('/politicas')
+def politicas():
+    return render_template('politicas.html', titulo="politicas")
+
+@app.route('/recuperarpass', methods=["GET","POST"])
+def recuperar():
+    formlogin = FormLogin()
+    formRestablecer = FormRestablecer()
+    if request.method == 'POST':
+        return render_template('login.html', titulo="login", form=formlogin)    
+    return render_template('recuperarpass.html', titulo="recuperar contraseña", form=formRestablecer)
+
+@app.route('/mainadmin', methods=["GET","POST"])
+def administrador():
+    if request.method == 'POST':
+        return render_template('mainadmin.html', titulo="main admininstrador")
+    return render_template('mainadmin.html', titulo="main admininstrador")
+
+@app.route('/mainadmin/crear')
+def crear():
+    formUser = FormUsuario()
+    return render_template('crearusuario.html', titulo="crear usuario", form=formUser)
+
+@app.route('/mainadmin/editar/<int:id>')
+def editar(id=1):
+    formUser = FormUsuario()
+    return render_template('editarusuario.html', titulo="editar usuario", id=id, form=formUser)
+
+@app.route('/mainadmin/evaluar/<int:id>')
+def evaluar(id):
+    formEvaluar = FormRetroalimentacion()
+    return render_template('evaluacion.html', titulo="evaluar usuario", id=id, form=formEvaluar)
+
+@app.route('/mainusuario/<int:id>')
+def usuario(id=1):
+    formFinalUser = FormUsuarioFinal()
+    return render_template('mainusuario.html', titulo="main usuario", id=id, form=formFinalUser)
+
+
+
+
---
+++ forms.py
@@ -0,0 +1,115 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, RadioField, SelectField
+from wtforms.validators import DataRequired,Required
+from wtforms.fields.html5 import EmailField, DateField, DecimalField,IntegerRangeField
+import pandas as pd
+from sodapy import Socrata 
+
+
+# Consultando la API del portal de datos abiertos de Colombia
+def ciudades():
+    client = Socrata("www.datos.gov.co", None)
+    result = client.get("xdk5-pm3f", limit = 2000)
+    df = pd.DataFrame.from_records(result)
+    cities = list(df["municipio"])
+    cities.append("        ---")
+    return sorted(cities)
+
+
+ciudades_op=ciudades()
+contrato_op = [('0', '      ---'), ('1', 'Fijo'), ('2', 'Indefinido'), ('3', 'Prestación de Servicios'), ('4', 'Aprendizaje'), ('5', 'Obra o Labor')]
+cargo_op = [('0', '      ---'), ('1', 'Directivo'), ('2', 'Jefatura'), ('3', 'Operativo'), ('4', 'Asistente'), ('5', 'Auxiliar')]
+dependencia_op = [('0', '      ---'), ('1', 'Mercadeo'), ('2', 'Comercial'), ('3', 'Talento Humano'), ('4', 'Producción'), ('5', 'Financiero'), ('6', 'Almacén')]
+roles = [('0', '      ---'), ('usuario', 'Usuario'), ('administrador', 'Administrador'), ('superAdministrador', 'Super Administrador')]
+
+
+class FormContacto(FlaskForm):
+    author = StringField('Nombre: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    email = StringField('Email: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    subject = StringField('Asunto: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    mensaje = TextAreaField('Mensaje: ')
+    enviar = SubmitField('Enviar')
+    
+class FormLogin(FlaskForm):
+    username = StringField('Usuario: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    password = PasswordField('Contraseña: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    politicas = BooleanField('Declaro haber leído y acepto las políticas de privacidad y protección de datos', validators=[Required(message='Debe seleccionar')])
+    submit = SubmitField('Ingresar')
+    
+class FormRestablecer(FlaskForm):
+    username = StringField('Usuario: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    restablecer = SubmitField('    Restablecer')
+    
+class FormUsuario(FlaskForm):
+    #datos personales
+    documento = StringField('Cédula: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    nombre = StringField('Nombre: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    apellidos = StringField('Apellidos: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    tipo_genero = RadioField('Genero:', choices=[('Masculino','M'),('Femenino','F')])
+    fechaNacimiento = DateField('Fecha Nacimiento:')
+    ciudadNacimiento = SelectField(u'Ciudad de Nacimiento:', choices=ciudades_op)
+    #acceso
+    user = StringField('Usuario: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    password = PasswordField('Contraseña: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    rol = SelectField(u'rol:', choices=roles)
+    #datos de contacto
+    telefono = StringField('Telefono: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    direccion = StringField('Dirección: ', validators=[DataRequired(message='No dejar vacío,completar')])
+    ciudadResidencia = SelectField(u'Ciudad de Residencia:', choices=ciudades_op)
+    email = EmailField('E-mail: ')
+    #datos de contrato
+    tipoContrato = SelectField(u'Tipo de Contrato:', choices=contrato_op)
+    cargo = SelectField(u'Cargo:', choices=cargo_op)
+    fechaInicio = DateField('Fecha Inicio:')
+    fechaFinalizacion = DateField('Fecha Finalización:')
+    dependencia = SelectField(u'Dependencia:', choices=dependencia_op)
+    salario = DecimalField('Salario: ')
+    estado = RadioField('Estado Contrato:', choices=[('activo','Activo'),('inactivo','Inactivo')])
+    
+    #Acciones
+    guardar = SubmitField('  Guardar')
+    editar = SubmitField('  Editar')
+    
+class FormRetroalimentacion(FlaskForm):
+    #fecha
+    fechaEvaluacion = DateField('Fecha de Evaluación:')
+    #empleado
+    documento = StringField('Cédula: ')
+    nombre = StringField('Nombre: ')
+    apellidos = StringField('Apellidos: ')
+    #retroalimentacion
+    puntaje = DecimalField('Puntaje Final: ')
+    retroalimentacion = TextAreaField('Comentarios: ')
+    #calificacion
+    conocimiento = IntegerRangeField('Conocimiento: ')
+    actitud = IntegerRangeField('Actitud: ')
+    habilidad = IntegerRangeField('Habilidad: ')
+    #Acciones
+    guardar = SubmitField('  Guardar')
+    editar = SubmitField('  Editar')
+        
+class FormUsuarioFinal(FlaskForm):
+    #datos personales
+    documento = StringField('Cédula: ')
+    nombre = StringField('Nombre: ')
+    apellidos = StringField('Apellidos: ')
+    tipo_genero = StringField('Genero')
+    fechaNacimiento = StringField('Fecha Nacimiento:')
+    ciudadNacimiento = StringField('Ciudad de Nacimiento:')
+    #datos de contacto
+    telefono = StringField('Telefono: ')
+    direccion = StringField('Dirección: ')
+    ciudadResidencia = StringField('Ciudad de Residencia: ')
+    email = StringField('E-mail: ')
+    #datos de contrato
+    contrato = SelectField(u'Seleccionar Contrato:', choices=contrato_op)
+    tipoContrato = StringField('Tipo de Contrato:')
+    cargo = StringField('Cargo:')
+    fechaInicio = StringField('Fecha Inicio:')
+    fechaFinalizacion = StringField('Fecha Finalización:')
+    dependencia = StringField('Dependencia:')
+    salario = StringField('Salario: ')
+    estado = StringField('Estado Contrato:')
+    #retroalimentacion
+    puntaje = StringField('Puntaje Final: ')
+    retroalimentacion = TextAreaField('Comentarios: ')
